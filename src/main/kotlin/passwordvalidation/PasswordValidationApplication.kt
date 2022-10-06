e3e4bb691fd7fbe71e3751f0b27892ee4a5271ab
yasmindias <yasmindias.info@gmail.com>
2022-10-05 22:01:51 -0300

    Cria estrutura inicial do projeto

---
+++ src/main/kotlin/passwordvalidation/PasswordValidationApplication.kt
@@ -0,0 +1,11 @@
+package passwordvalidation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PasswordValidationApplication
+
+fun main(args: Array<String>) {
+	runApplication<PasswordValidationApplication>(*args)
+}
---
+++ src/test/kotlin/passwordvalidation/PasswordValidationApplicationTests.kt
@@ -0,0 +1,13 @@
+package passwordvalidation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PasswordValidationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
