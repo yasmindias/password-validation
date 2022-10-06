d9ed6e26e87b358f1db3a5f2d69f244e43e5d462
yasmindias <yasmindias.info@gmail.com>
2022-10-06 14:15:00 -0300

    Adiciona validação com regex

--- src/main/kotlin/passwordvalidation/service/ValidationService.kt
+++ src/main/kotlin/passwordvalidation/service/ValidationService.kt
@@ -1,11 +1,21 @@
 package passwordvalidation.service
 
 import org.springframework.stereotype.Service
+import java.util.regex.Pattern
 
 @Service
 class ValidationService {
+    private val PATTERN = Pattern.compile(
+        "^" +
+                "(?=.*[a-z])" + //no mínimo uma letra minuscula
+                "(?=.*[A-Z])" + //no mínimo uma letra maiuscula
+                "(?=.*\\d)" + //no mínimo um dígito
+                "(?=.*[!@#\$%^&*()-+])" +  //no mínimo um caracter especial
+                "([A-Za-z\\d!@#\$%^&*()-+]{9,})" + //no mínimo 9 caracteres
+              "\$"
+    )
+
     fun validate(password: String): Boolean {
-        //TODO implementar Regex de verificação
-        return false
+        return PATTERN.matcher(password).matches()
     }
 }
